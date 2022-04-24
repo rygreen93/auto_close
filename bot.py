bcd53f22b2933acd44b9e7cc67fc1be561726b2f
rygreen93 <88078198+rygreen93@users.noreply.github.com>
2022-04-24 18:47:55 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,14 @@ import time
 import json
 import pprint
 from pybit import usdt_perpetual
+import os
+from colorama import Fore, Style
 
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
 
 file = open('api.json')
 data = json.load(file)
@@ -17,6 +24,8 @@ drawdown = 5
 
 while True:
     positions = session.my_position()
+    clearConsole()
+    print(f'{Fore.WHITE}Open positions:')
     for p in positions['result']:
         if float(p['data']['size']) > 0 or float(p['data']['size']) < 0:
             #(4006.33-7.32)*40468/4006.33*0.099
@@ -26,7 +35,16 @@ while True:
             #get Unrealized P%L considering leverage as well.
             #This is faster than ByBit frontend, so you will may experience some small differences.
             pl = (value-float(p['data']['position_value'])) / float(p['data']['position_value']) * 100 * float(p['data']['leverage'])
-
+            if float(p['data']['unrealised_pnl']) > 0:
+                if p['data']['side'] == 'Buy':
+                    print(f"{Fore.BLUE}{p['data']['symbol']}    {Fore.GREEN}BUY  {Fore.WHITE}DD: {Fore.GREEN}{round(pl,2)}%   {Fore.WHITE}Profit: {Fore.GREEN}{p['data']['unrealised_pnl']}")
+                else:
+                    print(f"{Fore.BLUE}{p['data']['symbol']}    {Fore.RED}SELL  {Fore.WHITE}DD: {Fore.GREEN}{round(pl,2)}%   {Fore.WHITE}Profit: {Fore.GREEN}{p['data']['unrealised_pnl']}")
+            else:
+                if p['data']['side'] == 'Buy':
+                    print(f"{Fore.BLUE}{p['data']['symbol']}    {Fore.GREEN}BUY  {Fore.WHITE}DD: {Fore.RED}{round(pl,2)}%  {Fore.WHITE}Profit: {Fore.RED}{p['data']['unrealised_pnl']}")
+                else:
+                    print(f"{Fore.BLUE}{p['data']['symbol']}    {Fore.RED}SELL  {Fore.WHITE}DD: {Fore.RED}{round(pl,2)}%  {Fore.WHITE}Profit: {Fore.RED}{p['data']['unrealised_pnl']}")
             if pl < drawdown*-1:
                 print('Close position')
                 if p['data']['side'] == 'Buy':
